b2cf92e2445fbb5d3359d7140db7fb16301a975f
Андрей Гришков <andreygorm@gmail.com>
2022-12-11 21:06:11 +0300

    add user-mesto

--- models/user.js
+++ models/user.js
@@ -60,4 +60,4 @@ userSchema.statics.findUserByCredentials = function (email, password) {
     });
 };
 
-module.exports = mongoose.model('user', userSchema);
+module.exports = mongoose.model('user-mesto', userSchema);
